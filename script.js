16accd5c7949de65384925b98b6b0a1bbb7c9e60
gulu43 <145833927+gulu43@users.noreply.github.com>
2025-02-11 11:06:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function Person(name, age) {
 	this.name = name;
 	this.age = age;
 	greet(){
-		console.log(`Hello, my name is ${name}, I am ${age} years old.`)
+		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`)
 	}
 }
 
@@ -13,7 +13,7 @@ function Employee(name, age, jobTitle) {
 	this.age = age;
 	this.jobTitle;
 	jobGreet(){
-		console.log(`Hello, my name is ${name}, I am ${age} years old, and my job title is ${jobTitle}.`)
+		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`)
 	}
 }
 Employee.prototype = Object.create(Person.prototype);
