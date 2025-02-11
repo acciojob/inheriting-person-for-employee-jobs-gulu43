6c824cbf29b5ff052389d3ae3a25933e8adccab3
gulu43 <145833927+gulu43@users.noreply.github.com>
2025-02-11 11:15:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,8 +9,6 @@ function Person(name, age) {
 
 function Employee(name, age, jobTitle) {
 	Person.call(this,name,age);
-	this.name = name;
-	this.age = age;
 	this.jobTitle;
 	jobGreet(){
 		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`)
